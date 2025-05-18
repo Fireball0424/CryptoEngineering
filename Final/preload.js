a4f194b2df58caf472bffa6e46598def5e0fb210
MuTing <a0979608003@gmail.com>
2025-05-18 21:27:45 +0800

    feat: Move final project file to another repo

--- Final/preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  login: (username, password) => ipcRenderer.invoke('login', { username, password })
-});
